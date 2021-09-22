6a18563dbdf307224fcc8f987e27cac339a3c956
chandramouli <codechandra@gmail.com>
2021-09-22 11:25:44 +0530

    Remove . code smell

--- src/decipherMessage.js
+++ src/decipherMessage.js
@@ -1,14 +1,16 @@
-const decipherSecretMessage=(key, message) => {
+const decipherSecretMessage = (key, message) => {
 
     let cipherKey = key.length;
     let decipheredMessage = "";
+    let aplhabetCount = 26;
+    let asciiValueOfA = 65;
     for (let i = 0; i < message.length; i++) {
         decipheredMessage += originalCharacter(message[i], cipherKey)
     }
-  //  console.log(key,message,decipheredMessage);
+    //  console.log(key,message,decipheredMessage);
     return decipheredMessage;
 }
-const originalCharacter=(letter, cipherLength) =>{
-    return String.fromCharCode((letter.charCodeAt(0) + (26 - cipherLength) - 65) % 26 + 65);
+const originalCharacter = (letter, cipherLength) => {
+    return String.fromCharCode((letter.charCodeAt(0) + (aplhabetCount - cipherLength) - asciiValueOfA) % aplhabetCount + asciiValueOfA);
 }
 module.exports = decipherSecretMessage;
