6fc87e4e4ef66b8dcc1a0e6c064bbdf09740154c
chandramouli <codechandra@gmail.com>
2021-09-29 12:35:15 +0530

    Modify . Refactor code

--- src/checkAlliance.js
+++ src/checkAlliance.js
@@ -2,44 +2,10 @@ const checkAlliance = (kingdom, secretMessage, emblem) => {
   const isKingdomAllied = require('./isKingdomAllied.js')
   let victories = 0
   const alliances = []
-  const Kingdoms = {
-    Land: 'LAND',
-    water: 'WATER',
-    Ice: 'ICE',
-    Air: 'AIR',
-    Fire: 'FIRE'
-  }
-  switch (kingdom) {
-    case Kingdoms.Land:
-      if (isKingdomAllied(emblem, secretMessage)) {
-        victories = victories + 1
-        alliances.push(Kingdoms.Land)
-      }
-      break
-    case Kingdoms.water:
-      if (isKingdomAllied(emblem, secretMessage)) {
-        victories = victories + 1
-        alliances.push(Kingdoms.water)
-      }
-      break
-    case Kingdoms.Ice:
-      if (isKingdomAllied(emblem, secretMessage)) {
-        victories = victories + 1
-        alliances.push(Kingdoms.Ice)
-      }
-      break
-    case Kingdoms.Air:
-      if (isKingdomAllied(emblem, secretMessage)) {
-        victories = victories + 1
-        alliances.push(Kingdoms.Air)
-      }
-      break
-    case Kingdoms.Fire:
-      if (isKingdomAllied(emblem, secretMessage)) {
-        victories = victories + 1
-        alliances.push(Kingdoms.Fire)
-      }
-      break
+
+  if(isKingdomAllied(emblem,secretMessage)){
+      victories=victories+1;
+      alliances.push(kingdom);
   }
   return [victories, alliances]
 }
